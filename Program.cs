d4bb517d08c34cbd733c457fd468cb81eeb5aaae
Raisa01~ <zarvirova@mail.ru>
2022-12-04 01:12:01 +0300

    Tasks 68

--- Program.cs
+++ Program.cs
@@ -21,7 +21,7 @@ int InputNumbers(string input)
 
 //Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 
-int m = InputNumbers("Введите m: ");
+/*int m = InputNumbers("Введите m: ");
 int n = InputNumbers("Введите n: ");
 int temp = m;
 
@@ -42,6 +42,29 @@ void PrintSumm(int m, int n, int summ)
     return;
   }
   PrintSumm(m, n - 1, summ);
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}*/
+
+//Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+
+int functionAkkerman = Ack(m, n);
+
+Console.Write($"Функция Аккермана = {functionAkkerman} ");
+
+int Ack(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return Ack(m - 1, 1);
+  else return Ack(m - 1, Ack(m, n - 1));
 }
 
 int InputNumbers(string input) 
